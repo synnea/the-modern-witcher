78e474f2962db88c547e0c7f2eb2072ed101b948
Carina <carrie.poell@gmail.com>
2020-01-02 19:20:51 +0100

    feat: add the various apps to the project

--- modern_witcher/settings.py
+++ modern_witcher/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-# import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -40,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'home',
+    'checkout',
+    'items'
 ]
 
 MIDDLEWARE = [
