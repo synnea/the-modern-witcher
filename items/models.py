dead820f44a0eff46088a1ae0680bacee284ab43
Carina <carrie.poell@gmail.com>
2020-01-03 16:07:42 +0100

    feat: add item model

--- items/models.py
+++ items/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+
+    ARMORER = 'Armor'
+    BLACKSMITH = 'Weapons'
+    HERBALIST = 'Ingredients'
+    OTHER = 'Other'
+
+    CATEGORY_CHOICES = [
+        (ARMORER, 'Armor'),
+        (BLACKSMITH, 'Weapons'),
+        (HERBALIST, 'Ingredients'),
+        (OTHER, 'Other')
+    ]
+
+    name = models.CharField(max_length=254, default='')
+    description = models.TextField()
+    
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default=OTHER,
+    )
+
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='item_images')
+
+    featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
