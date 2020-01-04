1653abb8dd692eff29bd36a94ca03719a3faf88a
Carina <carrie.poell@gmail.com>
2020-01-04 17:22:42 +0100

    feat: create and display cart page

---
+++ cart/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import view_cart
+
+urlpatterns = [
+    path('', view_cart, name="view_cart")
+]
--- modern_witcher/settings.py
+++ modern_witcher/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'checkout',
-    'items'
+    'items',
+    'cart'
 ]
 
 MIDDLEWARE = [
@@ -61,6 +62,7 @@ TEMPLATES = [
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
+            
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
