cadd4842a39169b5669df611609e1052aecf160a
Carina <carrie.poell@gmail.com>
2020-01-08 18:49:53 +0100

    feat: render detailed item view

--- items/views.py
+++ items/views.py
@@ -7,16 +7,9 @@ class ItemDetailView(DetailView):
     model = Item
     template_name = 'item_details.html'
     context_object_name = 'product'
-    extra_context = {}
     
     def get_object(self, queryset=Item):
         _id = int(self.kwargs.get('pk'))
-        print(_id)
-        instance = get_object_or_404(Item, id=_id)
-        self.extra_context['product'] = instance
-        print(instance)
-        return instance
+        product = get_object_or_404(Item, id=_id)
 
-# def get_detail(request, kwargs):
-#     product = get_object_or_404(Item, pk=kwargs)
-#     return render(request, 'item_details.html', {'product': product})
+        return product
