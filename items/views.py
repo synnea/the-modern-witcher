fd58310d7522d7d2b36edf49f6d794d479205cb9
Carina <carrie.poell@gmail.com>
2020-01-08 18:33:55 +0100

    feat: render item details correctly

--- items/views.py
+++ items/views.py
@@ -1,18 +1,21 @@
+from items.models import Item
 from django.shortcuts import render, get_object_or_404, redirect, reverse
-from .models import Item
 from django.views.generic.detail import DetailView
 
 class ItemDetailView(DetailView):
 
     model = Item
     template_name = 'item_details.html'
+    context_object_name = 'product'
+    extra_context = {}
     
     def get_object(self, queryset=Item):
         _id = int(self.kwargs.get('pk'))
         print(_id)
-        product = get_object_or_404(Item, pk=_id)
-        print(product)
-        return product
+        instance = get_object_or_404(Item, id=_id)
+        self.extra_context['product'] = instance
+        print(instance)
+        return instance
 
 # def get_detail(request, kwargs):
 #     product = get_object_or_404(Item, pk=kwargs)
