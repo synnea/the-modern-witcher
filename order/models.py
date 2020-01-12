c86c7e885a95bd4b281673599362cde8104d388d
Carina <carrie.poell@gmail.com>
2020-01-12 13:11:13 +0100

    feat: update models

--- order/models.py
+++ order/models.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import User
 from items.models import Item
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Item, on_delete=models.CASCADE, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    products = models.ManyToManyField(Item, on_delete=models.CASCADE, null=False)
     quantity = models.IntegerField(blank=False)
 
     def __str__(self):
-        return "{0} @ {1} {2}".format(self.user, self.product.name, self.quantity)
+        return "{0} @ {1} {2}".format(self.user, self.products.name, self.quantity)
+
+
+# len(carina.orders.products.filter(pk=item_to_review.pk)) == 0
+# view level
