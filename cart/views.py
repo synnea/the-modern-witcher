cb41d6c5dd4a46eb03bba12bf527b5a796c5ccc6
Carina <carrie.poell@gmail.com>
2020-01-13 13:18:43 +0100

    chore: remove redundant print statements

--- cart/views.py
+++ cart/views.py
@@ -9,10 +9,6 @@ def view_cart(request):
 
     if request.user.is_authenticated:
 
-        if request.session.get('amended'):
-            print("amended")
-            del request.session['amended']
-        
         try:
             current_user = request.user
             user = Profile.objects.get(username=current_user)
@@ -55,18 +51,18 @@ def add_to_cart(request, id):
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
 
+
 def amend_cart(request, id):
     """Adjust the quantity of the specified product to the specified amount."""
 
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
-    print(cart)
 
     if quantity > 0:
         cart[id] = quantity
     else:
         cart.pop(str(id))    
     
-    amended = True
-    request.session['amended'] = amended
+    messages.success(request, "Changes saved.")
+
     return redirect(reverse('view_cart'))
