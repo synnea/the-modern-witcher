939da60991d9ad4c75e1786f876373df1762f1e0
Carina <carrie.poell@gmail.com>
2020-01-14 17:23:48 +0100

    feat: get total display to work

--- cart/contexts.py
+++ cart/contexts.py
@@ -20,6 +20,6 @@ def cart_contents(request):
             quantity = int(quantity)
             total += quantity * product.price
             product_count += quantity
-            cart_items.append({'id': id, 'quantity': quantity, 'product': product})
+            cart_items.append({'id': id, 'quantity': quantity, 'product': product, 'total': total})
 
     return {'cart_items': cart_items, 'total': total, 'product_count': product_count}
