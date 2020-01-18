a671e1fcf6c37ecca5c3cc95f6fb5a0d9ce0df83
Carina <carrie.poell@gmail.com>
2020-01-18 13:27:04 +0100

    feat: create confirm url

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import view_cart, add_to_cart, amend_cart, view_payment
+from .views import view_cart, add_to_cart, amend_cart, view_payment, view_confirm
 
 urlpatterns = [
     path('', view_cart, name="view_cart"),
     path('add_to_cart/<int:id>', add_to_cart, name="add_to_cart"),
     path('amend_cart/<int:id>', amend_cart, name="amend_cart"),
     path('payment/', view_payment, name="view_payment"),
+    path('confirm/', view_confirm, name="view_confirm")
 ]
