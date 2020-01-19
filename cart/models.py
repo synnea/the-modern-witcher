f02eee246ab9738b6936808caf0ccc875552f9cd
Carina <carrie.poell@gmail.com>
2020-01-19 13:11:27 +0100

    debug: create order form

--- cart/models.py
+++ cart/models.py
@@ -8,13 +8,25 @@ class Order(models.Model):
 
     objects = models.Manager()
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
-    products = models.ManyToManyField(Item)
-    quantity = models.IntegerField(blank=False, default=0)
+
+    full_name = models.CharField(max_length=50, blank=False, default="Test")
+    phone_number = models.CharField(max_length=20, blank=False)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     date = models.DateField(default=datetime.date.today, null=True)
     
     def __str__(self):
-        return "{0} @ {1} {2}".format(self.user, self.products, self.quantity)
+        return "{0} @ {1}".format(self.user, self.date)
+
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, on_delete=models.PROTECT)
+    product = models.ForeignKey(Item, null=False, on_delete=models.PROTECT)
+    quantity = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return "{0} {1}".format(
+            self.quantity, self.product.name)
 
 
 # len(carina.orders.products.filter(pk=item_to_review.pk)) == 0
