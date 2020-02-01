d0590d8e2dbc241577e465d8fcc27e35a951c975
Carina <carrie.poell@gmail.com>
2020-02-01 17:02:47 +0100

    feat: complete navbar styling and behavior

--- static/js/fixed-navbar.js
+++ static/js/fixed-navbar.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     // Upon the document loading, assign two different navbar styles  to width below and above 800.
     // Below 800, assign a static top class. Above 800, assign a transparent background that picks up color after the user has scrolled for 20 pixels.
 
-        $("#navbar").removeClass("static-top").addClass("fixed-top");
+        $("#navbar").removeClass("static-top bg-color").addClass("fixed-top bg-transparent");
 
         $(window).scroll(function () {
             $("nav").toggleClass('scrolled', $(this).scrollTop() > 20);        });
