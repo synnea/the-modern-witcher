89007121868e107c40180fbd08bf87edeb08c564
Carina <carrie.poell@gmail.com>
2020-02-05 15:34:06 +0100

    style: finish review accordion

--- modern_witcher/settings.py
+++ modern_witcher/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import compressor
 import dj_database_url
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,6 +90,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'modern_witcher.wsgi.application'
 
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
