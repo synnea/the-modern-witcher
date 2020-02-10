811ef12ea7e368120dc242e95d8bf54acbd14f45
Carina <carrie.poell@gmail.com>
2020-02-10 13:13:03 +0100

    test: install coverage

---
+++ modern_witcher/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+class TestViews(TestCase):
+    def test_render_home_page(self):
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "index.html")
