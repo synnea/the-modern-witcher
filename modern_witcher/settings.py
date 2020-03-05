04c56dc15e6f6d555162e5c29973fe94c3ef9568
Carina <carrie.poell@gmail.com>
2020-03-05 13:53:07 +0100

    fix: uncomment PostgreSQL db

--- modern_witcher/settings.py
+++ modern_witcher/settings.py
@@ -97,15 +97,15 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+Database
+https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# if "DATABASE_URL" in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.getenv('DATABASE_URL') )
-#     }
-# else:
-#     print("Database URL not found. Using SQLite instead")
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.getenv('DATABASE_URL') )
+    }
+else:
+    print("Database URL not found. Using SQLite instead")
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
